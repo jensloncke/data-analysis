73b05045f42c6c42fa5527a2a098e7b2433b8255
jensloncke <jens.loncke@outlook.com>
2020-12-06 22:05:09 +0100

    added analysis file for confocal

---
+++ conf_microscopy_store_ER/Response_store_content_TrexRyR2_confocal.py
@@ -0,0 +1,30 @@
+from config import CONFIG
+import pandas as pd
+
+
+def analyse_column(column):
+    maximum = column.loc[CONFIG["start_max"]:CONFIG["stop_max"]].max()
+    minimum = column.loc[CONFIG["start_min"]:CONFIG["stop_min"]].min()
+    return maximum - minimum
+
+
+def analyse_data(path_read, path_write, filename):
+    df = pd.read_csv(path_read / filename, sep=",")
+    df.columns.values[0] = "Indices"
+    df["Tijd"] = 2 * df["Indices"] - 2
+    cols = df.columns.difference(['Indices', 'Label', 'Tijd'])
+    df[cols] = df[cols].div(df[cols].iloc[0])
+    df = df.set_index("Tijd")
+    columns_to_analyse = df[[column for column in df.columns if ("Mean" in column)]]
+    responses = pd.Series(index=columns_to_analyse.columns, name="response", dtype="float64")
+    for column_name, column in columns_to_analyse.iteritems():
+        responses[column_name] = analyse_column(column)
+    responses.to_csv(path_write / (filename[:-4] + "_responses.csv"), sep=";")
+
+
+if __name__ == "__main__": #1: define path 2: read in data
+    import os
+    path_analysis = CONFIG["path_analysis"]
+    path_response = CONFIG["path_response"]
+    os.makedirs(path_response, exist_ok=True)
+    analyse_data(path_analysis, path_response, CONFIG["filename"])
---
+++ conf_microscopy_store_ER/config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+CONFIG = {"path_analysis": Path("C:/Users/u0132307/Box Sync/PhD/Data/Ca2+_G-erCEPIA1_confocal/20201113-TrexERCEPIA"),
+          "path_response": Path("C:/Users/u0132307/Box Sync/PhD/Data/Ca2+_G-erCEPIA1_confocal/20201113-TrexERCEPIA/Response"),
+          "filename": "Results-KO1.csv",
+          "start_max": 5,
+          "stop_max": 90,
+          "start_min": 100,
+          "stop_min": 150}
